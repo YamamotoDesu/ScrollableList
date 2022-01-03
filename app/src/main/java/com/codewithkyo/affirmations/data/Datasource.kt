1624d84be09bf617bf5f8e23ac63b6d2c0e89e80
yamamoto <mynameisyamamotodesu@gmail.com>
2022-01-03 09:47:48 +0900

    Initial commit

---
+++ app/src/main/java/com/codewithkyo/affirmations/data/Datasource.kt
@@ -0,0 +1,22 @@
+package com.codewithkyo.affirmations.data
+
+import com.codewithkyo.affirmations.R
+import com.codewithkyo.affirmations.model.Affirmation
+
+class Datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
